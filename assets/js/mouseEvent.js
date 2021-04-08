bc47bc7581a55d54982b47fedf5298a6e25502a3
高ping战士 <31578556+monSteRhhe@users.noreply.github.com>
2021-04-08 19:38:29 +0800

    Update mouseEvent.js

--- assets/js/mouseEvent.js
+++ assets/js/mouseEvent.js
@@ -3,7 +3,7 @@ if(/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)) {}
 else {
     // click event
     $(document).click(function(event){
-        console.log(event.pageX + ' ' + event.pageY);
+        //console.log(event.pageX + ' ' + event.pageY);
     })
 
     // on document load
@@ -15,29 +15,28 @@ else {
         let posY = event.pageY - 25 - 2;
         $('.mouse-ring').attr('style', 'left: ' + posX + 'px; top: ' + posY + 'px; display: none;');
     })
-
-    // move event
-    $(document).mousemove(function(event){
-        let curadius = $('.mouse-ring').css('width').substring(0, 2) / 2 - 2;
-        //console.log(event.pageX + ' ' + event.pageY);
-        let posX = event.pageX - curadius;
-        let posY = event.pageY - curadius;
-        $('.mouse-ring').attr('style', 'left: ' + posX + 'px; top: ' + posY + 'px;');
-    });
-
-    $(document).scroll(function(event){
-        let curadius = $('.mouse-ring').css('width').substring(0, 2) / 2 - 2;
-        let posX = event.pageX - curadius;
-        let posY = event.pageY - curadius;
-        $('.mouse-ring').attr('style', 'left: ' + posX + 'px; top: ' + posY + 'px;');
-    });
-
-    $('a, .site-cont-bar').mousemove(function(){
-        $('.mouse-ring').addClass('on');
-    })
-
-    $('a, .site-cont-bar').mouseleave(function(){
-        $('.mouse-ring').removeClass('on');
-    })
 }
 
+// move event
+$(document).mousemove(function(event){
+    let curadius = $('.mouse-ring').css('width').substring(0, 2) / 2 - 2;
+    //console.log(event.pageX + ' ' + event.pageY);
+    let posX = event.pageX - curadius;
+    let posY = event.pageY - curadius;
+    $('.mouse-ring').attr('style', 'left: ' + posX + 'px; top: ' + posY + 'px;');
+});
+
+$(document).scroll(function(event){
+    let curadius = $('.mouse-ring').css('width').substring(0, 2) / 2 - 2;
+    let posX = event.pageX - curadius;
+    let posY = event.pageY - curadius;
+    $('.mouse-ring').attr('style', 'left: ' + posX + 'px; top: ' + posY + 'px;');
+});
+
+$('a, .site-cont-bar').mousemove(function(){
+    $('.mouse-ring').addClass('on');
+})
+
+$('a, .site-cont-bar').mouseleave(function(){
+    $('.mouse-ring').removeClass('on');
+})
